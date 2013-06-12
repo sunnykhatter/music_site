5d26fdf929dab24028fb1a077e8acd4f923ed4a0
Eric Szeto <eszeto@gmail.com>
2013-06-12 16:02:59 -0400

    database writing worked

--- webscraping.rb
+++ webscraping.rb
@@ -1,6 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
+require 'sqlite3'
 
-url = ""
+url = "http://en.wikipedia.org/wiki/List_of_highest-grossing_films"
 
 data = Nokogiri::HTML(open(url))
+
+
+begin 
+	db = SQLite3::Database.open "test1.db"
+	db.execute "CREATE TABLE IF NOT EXISTS Movies(Id INTEGER PRIMARY KEY, Name TEXT, Gross INT)"
+	db.execute "INSERT INTO Movies VALUES(1, 'Avatar', 100)"
+	db.execute "INSERT INTO Movies VALUES(2, 'Snatch', 99)"
+
+rescue SQLite3::Exception => e
+
+	puts "Exception occured"
+	puts e
+
+ensure
+	db.close if db
+end
