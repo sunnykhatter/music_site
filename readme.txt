ee3b076d6993ff2c866373201c703acdbe916244
Eric Szeto <eszeto@gmail.com>
2013-06-13 16:40:58 -0400

    fixed the title doubling

--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
 By Eric Szeto and Lakshay Khatter!
-Will change css.
+scrape_fetch.rb may not work depending on the computer.
+Use fetch.rb to run the site.
+--issue-- Nokogiri is scraping differently on our computers. Eric's has three new lines to split entries, Lakshays does not.
+Can use prepopulated Firsttop50.db and fetch.rb to run site though.
--- webscraping.rb
+++ webscraping.rb
@@ -13,19 +13,27 @@ begin
 	db.execute "CREATE TABLE IF NOT EXISTS Movies(Id INTEGER PRIMARY KEY, Name TEXT, Gross INTEGER, Year INTEGER)"
 	db.execute "DELETE FROM Movies"
 
-	table = data.css("html body div div div table")[1].text
-	# puts table
+	table = data.css("html body div div div table")[1]
+	titles = table.css("a").text
+	titles.slice!(0,4)
+	titles = titles.gsub(/\[. \d+\]/, "\n" )
+	title_array = titles.split("\n")
 
+	table = data.css("html body div div div table")[1].text
 	array = table.split(/\n\n\n/)
 	array.delete_at(0)
-	# rank = whole_table.css("tbody tr td").text.strip
+	count = 0
+
 	array.each do |string|
 		split_string = string.split(/\n/)
 		gross = split_string[2]
 		gross = gross.gsub("$", '')
 		gross = gross.gsub(/,/, '')
-		title = split_string[1].gsub(/'/, '')
+		title = title_array[count]
+		title = title.gsub(/'/, '')
+		# title = split_string[1].gsub(/'/, '')
 		db.execute "INSERT INTO Movies VALUES('#{split_string[0]}', '#{title}', '#{gross}', '#{split_string[3]}')"
+		count += 1
 	end
 
 rescue SQLite3::Exception => e
