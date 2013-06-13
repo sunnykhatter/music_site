c8c8c798a00d5b14499c883d0e3548f0e82f5cbd
Eric Szeto <eszeto@gmail.com>
2013-06-13 18:07:37 -0400

    changed font, added button color, button toggles text

--- public/myscript.js
+++ public/myscript.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 
   $("button.above-ten").click(function(){
     $(".overten").toggle();
+    if ($(this).text() == "Show Top 50") 
+    { 
+      $(this).text("Hide Top 50"); 
+    } 
+  	else 
+  	{ 
+     $(this).text("Show Top 50"); 
+  	}; 
   });
-
+  
 });
