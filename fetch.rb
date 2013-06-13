e1f567689d3c9344c8a852c9e7f80c84664b83e0
Eric Szeto <eszeto@gmail.com>
2013-06-13 08:51:38 -0400

    fixed the rescue in case of database failure

--- fetch.rb
+++ fetch.rb
@@ -3,27 +3,29 @@ require 'sinatra'
 
 get '/' do
 
-	# begin
+	begin
 
 
 	db = SQLite3::Database.open "top50.db"
 
 	stm = db.prepare "SELECT * FROM Movies LIMIT 50"
 	@rs = stm.execute
+	erb :movies
 
+	#just for displaying the rs array
 	# rs.each do |row|
 	# 	puts row.join(' ')
 	# end
 
-# rescue SQLite3::Exception => e 
-    
-#     puts "Exception occured"
-#     puts e
-    
-# # ensure
-#      stm.close if stm
-#      db.close if db
+	rescue SQLite3::Exception => e 
+	    
+	    puts "Exception occured"
+	    puts e
+	    
+	ensure
+	     stm.close if stm
+	     db.close if db
 
-# end
-erb :movies
+	end
+
 end
